5f928c1e01b4b88f3b1d80c7ce53bc8b44bb6dd5
gungkim <154861338+gungkim@users.noreply.github.com>
2024-04-16 09:40:35 +0900

    7차 시험 답안

---
+++ 7. 게임그래픽프로그래밍_PostProcess/Assets/Scripts/Test_PostProcess_이름.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Test_PostProcess_이름 : MonoBehaviour
+{
+    Volume volume;
+    Vignette vignette;
+    float direction;
+    float speed;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
